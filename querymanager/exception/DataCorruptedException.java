5922543366485f54a8f4605d372d32f22ef981d2
rcombresansq-pixel <rcombresansq@gmail.com>
2026-06-16 09:19:40 +0200

    feat: add custom exceptions

---
+++ querymanager/exception/DataCorruptedException.java
@@ -0,0 +1,8 @@
+package com.questmanager.exception;
+
+public class DataCorruptedException extends Exception {
+    public DataCorruptedException(String message) {
+        super(message);
+    }
+    
+}
---
+++ querymanager/exception/InvalidQuestException.java
@@ -0,0 +1,8 @@
+package com.questmanager.exception;
+
+public class InvalidQuestException extends Exception {
+
+    public InvalidQuestException(String message) {
+        super(message);
+    }
+}
