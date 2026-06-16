2e20508c9745588919c364cf135ae2868d9ccceb
rcombresansq-pixel <rcombresansq@gmail.com>
2026-06-16 09:48:51 +0200

    add save and load player mode

--- querymanager/repository/PlayerRepository.java
+++ querymanager/repository/PlayerRepository.java
@@ -4,6 +4,11 @@ import com.google.gson.Gson;
 
 import com.questmanager.model.Player;
 
+import java.io.FileWriter;
+
+import java.io.FileReader;
+
+
 public class PlayerRepository {
 
     private Gson gson;
@@ -16,3 +21,16 @@ public PlayerRepository(String filePath) {
     this.filePath = filePath;
 }
 
+public void save(Player player) throws Exception {
+    String json = gson.toJson(player);
+    FileWriter writer = new FileWriter(filePath);
+    writer.write(json);
+    writer.close();
+}
+
+public Player load() throws Exception {
+    FileReader reader = new FileReader(filePath);
+    Player player = gson.fromJson(reader, Player.class);
+    reader.close();
+    return player;
+}
