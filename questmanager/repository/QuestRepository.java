1c2ef59c398f7870e5e33b7b347ecb2c21df2d8b
rcombresansq-pixel <rcombresansq@gmail.com>
2026-06-16 10:27:23 +0200

    add quest list

--- questmanager/repository/QuestRepository.java
+++ questmanager/repository/QuestRepository.java
@@ -1,39 +1,32 @@
-package com.querymanager.repository;
+package com.questmanager.repository;
 
 import com.google.gson.Gson;
-
-import com.questmanager.model.Player;
-
-import java.io.FileWriter;
-
-import java.io.FileReader;
-
 import com.questmanager.model.Quest;
-
+import java.io.FileWriter;
+import java.io.FileReader;
 import java.util.List;
 
-
 public class QuestRepository {
-    public class PlayerRepository {
 
     private Gson gson;
     private String filePath;
 
-}
+    public QuestRepository(String filePath) {
+        this.gson = new Gson();
+        this.filePath = filePath;
+    }
 
-public QuestRepository(String filePath) {
-    this.gson = new Gson();
-    this.filePath = filePath;
-}
+    public void save(List<Quest> quests) throws Exception {
+        String json = gson.toJson(quests);
+        FileWriter writer = new FileWriter(filePath);
+        writer.write(json);
+        writer.close();
+    }
 
-public void save(Quest quest) throws Exception {
-    String json = gson.toJson(quest);
-    FileWriter writer = new FileWriter(filePath);
-    writer.write(json);
-    writer.close();
-}
-
-
-
-
+    public List<Quest> load() throws Exception {
+        FileReader reader = new FileReader(filePath);
+        List<Quest> quests = gson.fromJson(reader, List.class);
+        reader.close();
+        return quests;
+    }
 }
